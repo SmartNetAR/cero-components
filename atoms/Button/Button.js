3c33badec904896bd539788945daf03285bd4b08
Leonardo Casales <leonardo@smartnet.com.ar>
2021-10-02 20:32:40 -0300

    button primary first styles

--- atoms/Button/Button.js
+++ atoms/Button/Button.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import './Button.css'
 
 export const handleClick = ({ onClick }) => (event) => {
     onClick(event)
--- stories/atoms/Button.stories.js
+++ stories/atoms/Button.stories.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Button from '../../atoms/Button'
 
 export default {
-    title: 'Atoms/Button',
+    title: 'Atoms/Buttons',
     component: Button,
 }
 
