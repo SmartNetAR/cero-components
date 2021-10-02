674fc400732c5abb0b6beba4cf6ad662b162f886
Leonardo Casales <leonardo@smartnet.com.ar>
2021-10-02 19:45:22 -0300

    first commit

---
+++ atoms/Button/Button.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export const handleClick = ({ onClick }) => (event) => {
+    onClick(event)
+}
+
+const Button = ({
+    children,
+    onClick,
+}) => (
+    <button
+        onClick={handleClick({ onClick })}
+    >
+        {children}
+    </button>
+)
+
+Button.propTypes = {
+    children: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+}
+
+Button.defaultProps = {
+    onClick: () => { },
+}
+
+export default Button
---
+++ stories/atoms/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Button from '../../atoms/Button'
+
+export default {
+    title: 'Atoms/Button',
+    component: Button,
+}
+
+export const Primary = () => <Button>Primary</Button>
