60a1bb759e85581c13c5ec07d19a8298b366d2bd
Leonardo Casales <leonardo@smartnet.com.ar>
2021-10-02 22:15:11 -0300

    feat : jest onClick prop test

---
+++ atoms/Button/Button.test.js
@@ -0,0 +1,23 @@
+import React from 'react' ;
+import { render, fireEvent, screen } from '@testing-library/react' ;
+
+import Button, { handleClick } from './Button' ;
+
+describe( '[ atoms / Button ]', () => {
+    describe( '#Button', () => {
+        it( 'when `Button` is clicked', () => {
+            // Arrange
+            const onClickMock = jest.fn() ;
+            const props = {
+                onClick: onClickMock,
+            } ;
+
+            // Act
+            render( <Button { ...props }>Button</Button> ) ;
+            fireEvent.click( screen.getByText( 'Button' ) ) ;
+
+            // Assert
+            expect( onClickMock ).toHaveBeenCalled() ;
+        } ) ;
+    } ) ;
+} ) ;
